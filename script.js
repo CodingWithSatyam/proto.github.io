4254de10d3ada04a9597f8df00d1de4c5cbeec74
Coding With Satyam <techratic@yahoo.com>
2022-04-29 08:03:54 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+  // toggle menu
+  $(`.menu-btn`).click(function () {
+    $(`.navbar .menu`).toggleClass("action");
+    $(`.menu-btn i`).toggleClass("action");
+  });
+//   typing Animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
